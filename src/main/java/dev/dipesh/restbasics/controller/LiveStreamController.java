f932c4614478f2d7bb12c5c3b633ca15fd1f23a3
Dipesh Khadgi <Dipesh0221@gmail.com>
2022-10-16 14:05:28 +0545

    completed controller methods

--- src/main/java/dev/dipesh/restbasics/controller/LiveStreamController.java
+++ src/main/java/dev/dipesh/restbasics/controller/LiveStreamController.java
@@ -2,10 +2,13 @@ package dev.dipesh.restbasics.controller;
 
 import java.util.List;
 
+import org.apache.catalina.startup.ClassLoaderFactory.Repository;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -45,4 +48,15 @@ public class LiveStreamController {
     public Livestream createStream(@RequestBody Livestream stream){
         return liveStreamRepository.create(stream);
     }
+
+    @PutMapping("/{id}")
+    public void updateStream(@RequestBody Livestream stream, @PathVariable String id){
+        liveStreamRepository.update(stream, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStream(@PathVariable String id) {
+        liveStreamRepository.delete(id);
+    }
+
 }
